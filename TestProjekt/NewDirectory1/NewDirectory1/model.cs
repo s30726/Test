182b83cb457cb6a2104515efa96351417db561e5
karolkubica <s30726@pjwstk.edu.pl>
2026-03-24 14:48:40 +0100

    testing if dir works

---
+++ TestProjekt/NewDirectory1/NewDirectory1/model.cs
@@ -0,0 +1,6 @@
+﻿namespace TestProjekt.NewDirectory1.NewDirectory1;
+
+public class model
+{
+    
+}
---
+++ TestProjekt/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace TestProjekt;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
