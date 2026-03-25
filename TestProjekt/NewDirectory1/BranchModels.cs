d1902b851b1ca3a3742cdda8909777749b6c5bd0
karolkubica <s30726@pjwstk.edu.pl>
2026-03-25 14:11:10 +0100

    testing if branch works

---
+++ TestProjekt/NewDirectory1/BranchModels.cs
@@ -0,0 +1,11 @@
+﻿namespace TestProjekt.NewDirectory1;
+
+public class BranchModels
+{
+    private int number;
+    public BranchModels(int number)
+    {
+        this.number = number;
+    }
+    
+}
